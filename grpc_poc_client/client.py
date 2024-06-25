e7a13574c10ae15ec0603ca38c8fe49ea6708e82
Adrian Borella <aborella@kindalab.co>
2024-06-25 14:44:26 +0200

    host and port configurable

--- grpc_poc_client/client.py
+++ grpc_poc_client/client.py
@@ -10,6 +10,18 @@ def sumFunction(num1, num2):
     print(f"Result: {response.result}")
 
 
+class GrpcPocClient:
+    def __init__(self, host, port):
+        self._host = host
+        self._port = port
+
+    def sumFunction(self, num1, num2):
+        with grpc.insecure_channel(f"{self._host}:{self._port}") as channel:
+            stub = calculator_pb2_grpc.CalculatorStub(channel)
+            response = stub.Add(calculator_pb2.AddRequest(num1=num1, num2=num2))
+            return response
+
+
 if __name__ == "__main__":
     # Get user Input
     num1 = int(input("Please input num1: "))
